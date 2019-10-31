3066ec25c49e5d54e80ab50be88bd31f7018b5f8
Johnson Wei <joeywei@cpp.edu>
2019-10-30 21:52:19 -0700

    added initial files to get started on pipeline project

---
+++ SimpleProject/src/main/java/com/cs5800/simple/SimpleClass.java
@@ -0,0 +1,13 @@
+package main.java.com.cs5800.simple;
+
+public class SimpleClass {
+	private int number;
+	
+	SimpleClass(int number){
+		this.number = number;
+	}
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+}
---
+++ SimpleProject/src/test/java/com/cs5800/simple/SimpleTest.java
@@ -0,0 +1,14 @@
+package test.java.com.cs5800.simple;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
